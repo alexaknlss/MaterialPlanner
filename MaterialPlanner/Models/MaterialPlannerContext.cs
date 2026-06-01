4821a6b4a7961b2d37fe92b51f92f502f195bfa0
triminiodiego <diego.triminio@unah.hn>
2026-06-01 14:18:15 -0600

    Conexión a base de datos con EF Core

---
+++ MaterialPlanner/Models/MaterialPlannerContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaterialPlanner.Models;
+
+public partial class MaterialPlannerContext : DbContext
+{
+    public MaterialPlannerContext()
+    {
+    }
+
+    public MaterialPlannerContext(DbContextOptions<MaterialPlannerContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=MaterialPlanner;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
